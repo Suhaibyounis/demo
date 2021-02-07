e3737d4ab2e66e3be3879d08921b25fad08a9943
suhaibyounis <Younissuhaib@yahoo.com>
2021-02-07 23:31:28 +0200

    add app.js and index file

---
+++ js/app.js
@@ -0,0 +1,29 @@
+console.log('student name');
+
+var fname= 'Ghazl-BYounis';
+
+console.log('First name: ' + fname );
+console.log(fname);
+
+var age= '2'
+console.log(age);
+
+ var isStudent= true
+ console.log(isStudent);
+
+console.log(fname + ' , ' + age + ' years old, is student ' + isStudent)
+
+console.log(19+'2'+2019)
+
+var firstname =  prompt('whats your name?' , 'enter your name');
+console.log(firstname)
+
+if(isStudent == false){
+    console.log('he is not a student')
+} else {
+    console.log ('he is a student')
+}
+
+if (firstname == 'Ghazl'){
+    console.log ( 'hello ' + firstname);
+}
