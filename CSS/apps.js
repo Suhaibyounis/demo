2c0bf827688b6c324ea3a41b90a7e5ec7a1c43b1
suhaibyounis <Younissuhaib@yahoo.com>
2021-02-09 14:52:51 +0200

    make new folder CSS

---
+++ CSS/apps.js
@@ -0,0 +1,12 @@
+alert('Welcome to Jordan')
+
+var sayHello = prompt ('what is your nationality?');
+
+function printWelcoming(){
+    document.write('Welcome to you' +  ` ${sayHello}` + ', You are in Jordan.');
+    
+}
+
+printWelcoming()
+
+
